d4f6862d1436d5857b46bf28b52b4b47fe1bd108
Galina Zelisko <AnnaZelisko@Galinas-MacBook-Pro.local>
2017-03-12 11:51:48 -0400

    Added to Main and modified OrderTest.java

--- project/src/Group/Main.java
+++ project/src/Group/Main.java
@@ -9,11 +9,14 @@ public class Main {
 	 public static void main(String[] args){
 		// Buffered Read is a java system used to read in files
 		// try statement because sometimes file can't be found 
-		String fileWithOrders = "/Users/AnnaZelisko/Documents/group_0406/project/16orders.txt";
-		String fileSKUs = "/Users/AnnaZelisko/Documents/group_0406/project/translation.csv";
+		String fileWithSteps = "/Users/AnnaZelisko/Documents/group_0406/project/16orders.txt";
+		String fileWithSKUs = "/Users/AnnaZelisko/Documents/group_0406/project/translation.csv";
+		String fileWithWarehouseInfo = "/Users/AnnaZelisko/Documents/group_0406/project/traversal_table.csv";
+		SKUReader SKUFile = new SKUReader(fileWithSKUs);
+		Warehouse WarehouseFile = new Warehouse(fileWithWarehouseInfo);
 		
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(fileWithOrders));
+			BufferedReader br = new BufferedReader(new FileReader(fileWithSteps));
 			//local variables to help us track and control each read in line
 			String line;
 			// this is to help figure and break down the read in line
@@ -24,15 +27,14 @@ public class Main {
 				// we split into the string array based on spaces
 				lineParts = line.split(" ");
 	
-				
 				// if its an order request, I used matches because if there is a space or miss type dont wanna risk == failing
 				if (lineParts[0].matches("Order")){				
-					new Order(lineParts[2], lineParts[1], fileSKUs);					
+					new Order(lineParts[2], lineParts[1]);					
 				}
 				// if its an picker request
 				else if (lineParts[0].matches("Picker")){
-					new Worker();
-					// we know that fields lineParts[1] has a name and lineParts[2], lineParts[3] has some status info 
+					new Worker(lineParts[1]);
+					// we know that fields  has a name and lineParts[2], lineParts[3] has some status info 
 				}	
 				// if its an picker request
 				else if (lineParts[0].matches("Sequencer")){
--- project/src/Group/OrderTest.java
+++ project/src/Group/OrderTest.java
@@ -5,12 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class OrderTest {
-
-	String fileSKUs = "/Users/AnnaZelisko/Documents/group_0406/project/translation.csv";
 	
 	@Test
 	public void Test1CreateOrder() {
-		Order o1 = new Order("red","XL", fileSKUs);
+		Order o1 = new Order("red","XL");
 		assertEquals(o1.colour, "red");
 		assertEquals(o1.model, "XL");
 	}
